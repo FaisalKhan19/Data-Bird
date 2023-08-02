6ed836d20f234e18451cfc5bf4ddba0dc8089a30
FaisalKhan19 <102422412+FaisalKhan19@users.noreply.github.com>
2023-08-03 01:11:40 +0530

    some debugging

--- Main/driver.py
+++ Main/driver.py
@@ -20,7 +20,7 @@ def perform_web_automation(driver):
     if driver is not None:
         try:
             # Open a webpage
-            driver.get('https://www.screener.in/company/524774/')
+            driver.get('https://www.tickertape.in/stocks/tata-consultancy-services-TCS')
 
             # Add a JavaScript click event listener to the window
             driver.execute_script("""
@@ -50,9 +50,6 @@ def main():
     root = tk.Tk()
     root.title("Web Automation Example")
 
-    scrape_button = tk.Button(root , text = 'Scrape',command=start_scraping,font =("Helvetica",14),fg= 'grey' )
-    # scrape_buttonvscode-file://vscode-app/c:/Users/asus/AppData/Local/Programs/Microsoft%20VS%20Code/resources/app/out/vs/code/electron-sandbox/workbench/workbench.html
-    indicate_on_screen  =tk.Button(root,text = 'Indicate on Screen',command = Indicate)
     driver = create_webdriver()
 
     perform_web_automation(driver)
